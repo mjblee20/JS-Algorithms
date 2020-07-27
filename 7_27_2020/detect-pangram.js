045e35eb94f2a37c312ab8d66fca720243b2f10b
MonteCristo <mjblee20@gmail.com>
2020-07-27 16:23:20 -0700

    7/27/2020 4:30pm

--- 7_27_2020/detect-pangram.js
+++ 7_27_2020/detect-pangram.js
@@ -7,3 +7,27 @@
  * Given a string, detect whether or not it is a pangram. Return True if it is, 
  * False if not. Ignore numbers and punctuation.
  */
+
+function isPangram(string) {
+  // create an array of alphabets and pop each alphabet off as they appear
+  var alphabets = "abcdefghijklmnopqrstuvwxyz";
+  // take string lower case it and split it
+  var test = string.toLowerCase();
+  // loop through the string
+  for (let i = 0; i <= test.length; i++) {
+    //If the letter is present in the array, remove it
+    if (alphabets.indexOf(test[i]) !== -1) {
+        alphabets = alphabets.replace(test[i], '') 
+        
+    } 
+  }
+
+  // if alphabets array has nothing in it then return true
+  console.log(alphabets.length === 0);
+  return alphabets.length === 0;
+}
+
+var string1 = "The quick brown fox jumps over the lazy dog."
+isPangram(string1);
+var string2 = "This is not a pangram."
+isPangram(string2);
